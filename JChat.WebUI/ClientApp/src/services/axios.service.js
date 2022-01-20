cb1c81709abed7cb0486127f48f8fc5c276382f6
Alan Weingartner <hi@alanwgt.com>
2022-01-20 19:45:42 -0300

    fix: don't touch the axios request sent to kratos

--- JChat.WebUI/ClientApp/src/services/axios.service.js
+++ JChat.WebUI/ClientApp/src/services/axios.service.js
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+
 import store from '@/store';
 
 const axios = Axios.create({
@@ -6,6 +7,12 @@ const axios = Axios.create({
 });
 
 axios.interceptors.request.use((config) => {
+  // don't touch the request sent to kratos
+  // TODO: get domain from env
+  if (config.url.includes('kratos.jchat.alanwgt.com')) {
+    return config;
+  }
+
   const newConf = { ...config };
   const id = store.getState().workspace?.id;
 

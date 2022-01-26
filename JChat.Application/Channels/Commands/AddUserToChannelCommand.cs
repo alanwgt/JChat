53a862f0b545a59639b67a27750d87b6eeae8500
Alan Weingartner <hi@alanwgt.com>
2022-01-26 18:37:17 -0300

    fix: remove unnecessary transaction

--- JChat.Application/Channels/Commands/AddUserToChannelCommand.cs
+++ JChat.Application/Channels/Commands/AddUserToChannelCommand.cs
@@ -45,34 +45,20 @@ public class AddUserToChannelCommandHandler : IRequestHandler<AddUserToChannelCo
 
     public async Task<ChannelUserBriefDto> Handle(AddUserToChannelCommand request, CancellationToken cancellationToken)
     {
-        var channel = await _context.Channels.FindAsync(new object?[] { request.ChannelId }, cancellationToken);
-        await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
-
-        if (channel == null)
-            throw new NotFoundException("channel", request.ChannelId);
+        var channel = await _context.Channels.FindAsync(new object?[] { request.ChannelId }, cancellationToken) ??
+                      throw new NotFoundException("channel", request.ChannelId);
 
         var channelUser = channel.AddUser(request.UserId);
 
-        try
-        {
-            await _context.ChannelUsers.AddAsync(channelUser, cancellationToken);
+        await _authorization.Authorize(
+            AuthzNamespace.Channels.Str(),
+            channel.Id.ToString(),
+            AuthzRelation.Member.Str(),
+            request.UserId.ToString(),
+            cancellationToken
+        );
 
-            await _authorization.Authorize(
-                AuthzNamespace.Channels.Str(),
-                channel.Id.ToString(),
-                AuthzRelation.Member.Str(),
-                request.UserId.ToString(),
-                cancellationToken
-            );
-
-            await transaction.CommitAsync(cancellationToken);
-
-            return _mapper.Map<ChannelUserBriefDto>(channelUser);
-        }
-        catch
-        {
-            await transaction.RollbackAsync(cancellationToken);
-            throw;
-        }
+        await _context.SaveChangesAsync(cancellationToken);
+        return _mapper.Map<ChannelUserBriefDto>(channelUser);
     }
 }

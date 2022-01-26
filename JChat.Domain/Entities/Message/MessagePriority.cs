a727835537c0535af11092c7a6f230812c041ca1
Alan Weingartner <hi@alanwgt.com>
2022-01-26 18:31:21 -0300

    fix: change column type to smallint
    add priority in constructor

--- JChat.Domain/Entities/Message/MessagePriority.cs
+++ JChat.Domain/Entities/Message/MessagePriority.cs
@@ -7,15 +7,16 @@ namespace JChat.Domain.Entities.Message;
 public class MessagePriority : Entity
 {
     public string Name { get; protected set; }
-    [Column(TypeName = "char")]
+    [Column(TypeName = "smallint")]
     public MessagePriorityType Priority { get; protected set; }
 
     protected MessagePriority()
     {
     }
 
-    public MessagePriority(string name)
+    public MessagePriority(string name, MessagePriorityType priority)
     {
         Name = name;
+        Priority = priority;
     }
 }
